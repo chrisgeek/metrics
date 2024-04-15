7397871a738ef3c8e62733c08418e2b19144e8a5
Opara Ifeanyi Christian <chrisgeek29@gmail.com>
2024-04-15 14:46:08 +0200

    docker setup

--- metrics_api/db/schema.rb
+++ metrics_api/db/schema.rb
@@ -10,17 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_07_121046) do
+ActiveRecord::Schema[7.1].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "metrics", force: :cascade do |t|
-    t.datetime "timestamp"
-    t.decimal "value", precision: 10, scale: 2
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["timestamp", "name"], name: "index_metrics_on_timestamp_and_name"
-  end
 
 end
